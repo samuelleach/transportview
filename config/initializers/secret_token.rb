f45249c12014739de4b02677e7d791f5b58cfde9
Samuel Leach <samuel.leach@gmail.com>
2013-05-11 19:11:29 +0100

    New rails project with rails new . --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transportview::Application.config.secret_token = '64cf58a5c1eaf76a6e3b5dd1432a72fd7ba1916f3a9ca000b7a5353b47fe424388c861c6dc0f1a5f2746041914df13a8d4181713aa94727cf151d2feccae20c6'
