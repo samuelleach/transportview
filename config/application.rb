ffcb7badd33893c9743141baf6733dc6fe4c6b80
Samuel Leach <samuel.leach@gmail.com>
2013-05-14 21:24:30 +0100

    Preparing to deploy to Heroku

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Transportview
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # For Heroku deployment
+    config.assets.initialize_on_precompile = false
   end
 end
